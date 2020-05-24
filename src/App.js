00804c178a105cddf4cea32935afbdb98e329bd3
Ashish Karki <4175758+ashishkarki@users.noreply.github.com>
2020-05-24 19:21:44 +0545

    moved menu-item and directory to their own components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
-
-import HomePage from './home-page.component'
+import HomePage from './pages/home-page/home-page.component'
 
 function App() {
   return (
