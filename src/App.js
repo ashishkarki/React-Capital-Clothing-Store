30fc631bbf79cfb5310d9a381bd3a9d9a84faebb
Ashish Karki <4175758+ashishkarki@users.noreply.github.com>
2020-05-27 14:03:34 +0545

    Implemented header component with its styles. More links to be added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
+
 import './App.css'
+
 import HomePage from './pages/home-page/home-page.component'
 import ShopPage from './pages/shop/shop.component'
+import Header from './components/header/header.component'
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
